18344ee356f06bc9fab958bc1804830c5375046b
Enderlook <enderlook@gmail.com>
2020-06-06 15:10:48 -0300

    Add Explosive Ammunition

---
+++ Assets/Scripts/Player/Attachs/ExplodeOnHitAttach.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Ammunitions
+{
+    [CreateAssetMenu(fileName = "Explode On Hit", menuName = "Game/Cannon/Attach/Explode On Hit")]
+    public class ExplodeOnHitAttach : Attach
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Damage done over distance.")]
+        private float damageOverDistance = 1;
+
+        [SerializeField, Tooltip("Force applied on explosion.")]
+        private float explosionForce = 1;
+
+        [SerializeField, Tooltip("Minimal amount of damage required to produce it.")]
+        private float damageThreshold = .25f;
+#pragma warning restore CS0649
+
+        public override void Accept(GameObject gameObject) => ExplodeOnHit.AddComponentTo(gameObject, explosionForce, damageOverDistance, damageThreshold);
+    }
+}
---
+++ Assets/Scripts/Player/Components/ExplodeOnHit.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Game.Ammunitions
+{
+    public class ExplodeOnHit : MonoBehaviour
+    {
+        private float damageOverDistance;
+
+        private float explosionForce;
+
+        private float damageThreshold;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, 5))
+            {
+                if (collider.gameObject == gameObject)
+                    continue;
+
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                if (collider.TryGetComponent(out Rigidbody2D rigidbody2D))
+                    rigidbody2D.AddForceAtPosition(direction * explosionForce, transform.position);
+                float damage = damageOverDistance / direction.magnitude;
+                if (damage > damageThreshold && collider.TryGetComponent(out Hurtable hurtable))
+                    hurtable.TakeDamage(damageOverDistance / direction.magnitude);
+            }
+
+            Destroy(gameObject);
+        }
+
+        public static void AddComponentTo(GameObject gameObject, float explosionForce, float damageOverDistance, float damageThreshold)
+        {
+            ExplodeOnHit component = gameObject.AddComponent<ExplodeOnHit>();
+            component.explosionForce = explosionForce;
+            component.damageOverDistance = damageOverDistance;
+            component.damageThreshold = damageThreshold;
+        }
+    }
+}
