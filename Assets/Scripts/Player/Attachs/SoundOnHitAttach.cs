b5bc197a0305585aef22ebbecf538b2e85193953
Enderlook <enderlook@gmail.com>
2020-06-06 15:38:46 -0300

    Add sound on hit projectiles

---
+++ Assets/Scripts/Player/Attachs/SoundOnHitAttach.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Game.Ammunitions
+{
+    [CreateAssetMenu(fileName = "Sound On Hit", menuName = "Game/Cannon/Attach/Sound On Hit")]
+    public class SoundOnHitAttach : Attach
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Sound played on hit.")]
+        private AudioClip audioClip;
+
+        [SerializeField, Tooltip("Audio Mixer Group used to play sound.")]
+        private AudioMixerGroup audioMixerGroup;
+#pragma warning restore CS0649
+
+        public override void Accept(GameObject gameObject) => SoundOnHit.AddComponentTo(gameObject, audioClip, audioMixerGroup);
+    }
+}
---
+++ Assets/Scripts/Player/Components/SoundOnHit.cs
@@ -0,0 +1,33 @@
+﻿using Enderlook.Unity.Components.Destroy;
+
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Game.Ammunitions
+{
+    public class SoundOnHit : MonoBehaviour
+    {
+        private AudioClip audioClip;
+        private AudioMixerGroup audioMixerGroup;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            GameObject sound = new GameObject();
+            AudioSource audioSource = sound.AddComponent<AudioSource>();
+            audioSource.outputAudioMixerGroup = audioMixerGroup;
+            audioSource.clip = audioClip;
+            audioSource.pitch = Random.Range(.8f, 1.2f);
+            audioSource.volume = Random.Range(.8f, 1f);
+            audioSource.Play();
+            DestroyWhenAudioSourceEnds.AddComponent(sound);
+        }
+
+        public static void AddComponentTo(GameObject gameObject, AudioClip audioClip, AudioMixerGroup audioMixerGroup)
+        {
+            SoundOnHit component = gameObject.AddComponent<SoundOnHit>();
+            component.audioClip = audioClip;
+            component.audioMixerGroup = audioMixerGroup;
+        }
+    }
+}
