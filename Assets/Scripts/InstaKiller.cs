54c08695da7c814ff726441e0f6699b455656932
Enderlook <enderlook@gmail.com>
2020-06-06 16:38:04 -0300

    Fix InstaKiller

--- Assets/Scripts/InstaKiller.cs
+++ Assets/Scripts/InstaKiller.cs
@@ -5,10 +5,6 @@ namespace Game
     public class InstaKiller : MonoBehaviour
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.TryGetComponent(out IDamagable damagable))
-                damagable.Kill();
-        }
+        private void OnCollisionEnter2D(Collision2D collision) => Destroy(collision.gameObject);
     }
 }
