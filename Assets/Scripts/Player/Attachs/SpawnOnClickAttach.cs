c9b0a8ce80517456af35600726ea698c09532ecd
Enderlook <enderlook@gmail.com>
2020-06-20 16:31:32 -0300

    Replace SpawnOnHit with SpawnOnClick

---
+++ Assets/Scripts/Player/Attachs/SpawnOnClickAttach.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Ammunitions
+{
+    [CreateAssetMenu(fileName = "Spawn On Click", menuName = "Game/Cannon/Attach/Spawn On Click")]
+    public class SpawnOnClickAttach : Attach
+    {
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Ammunitions spawn.")]
+        private Ammunition[] ammunitions;
+
+        [SerializeField, Tooltip("Amount randomization of force.")]
+        private float randomization;
+
+        [SerializeField, Tooltip("Amount of force inherited by pellets.")]
+        private float inheritForceRatio = 1;
+#pragma warning restore CS0649
+
+        public override void Accept(GameObject gameObject) => SpawnOnClick.AddComponentTo(gameObject, ammunitions, randomization, inheritForceRatio);
+    }
+}
---
+++ Assets/Scripts/Player/Components/SpawnOnClick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Game.Ammunitions
+{
+    public class SpawnOnClick : MonoBehaviour
+    {
+        private Ammunition[] ammunitions;
+
+        private float randomization;
+
+        private float inheritForceRatio;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Update()
+        {
+            if (!Input.GetMouseButtonDown(1))
+                return;
+
+            if (ammunitions.Length == 0)
+                return;
+
+            Ammunition ammunition = ammunitions[0];
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            ammunition.Shoot(rigidbody.velocity * rigidbody.mass * inheritForceRatio, transform.position);
+
+            for (int i = 0; i < ammunitions.Length - 1; i++)
+            {
+                Vector2 random = (Vector2.one * (1 - randomization)) + ((Vector2)Random.onUnitSphere * randomization);
+                ammunitions[i].Shoot(rigidbody.velocity * rigidbody.mass * inheritForceRatio * random, transform.position);
+            }
+            Destroy(gameObject);
+        }
+
+        public static void AddComponentTo(GameObject gameObject, Ammunition[] ammunitions, float randomization, float inheritForceRatio)
+        {
+            SpawnOnClick component = gameObject.AddComponent<SpawnOnClick>();
+            component.ammunitions = ammunitions;
+            component.randomization = randomization;
+            component.inheritForceRatio = inheritForceRatio;
+        }
+    }
+}
